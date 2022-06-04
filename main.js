641c0608389265728c65d6d73c603c7937f4f687
JosueMatos378 <77208952+JosueMatos378@users.noreply.github.com>
2022-06-04 15:23:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function onScroll() {
+  var element = document.getElementById("navigation")
+
+  if(scrollY > 0) {
+    element.classList.add("scroll")
+  }
+  else {
+    element.classList.remove("scroll")
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded")
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded")
+}
