ab08842bccfb2291ab53821e51dc8fdd224ec9ba
JosueMatos378 <77208952+JosueMatos378@users.noreply.github.com>
2022-06-11 15:45:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 function onScroll() {
-  var element = document.getElementById("navigation")
+  var element = document.getElementById('navigation');
 
-  if(scrollY > 0) {
-    element.classList.add("scroll")
-  }
-  else {
-    element.classList.remove("scroll")
+  if (scrollY > 0) {
+    element.classList.add('scroll');
+  } else {
+    element.classList.remove('scroll');
   }
 }
 function openMenu() {
-  document.body.classList.add("menu-expanded")
+  document.body.classList.add('menu-expanded');
 }
 function closeMenu() {
-  document.body.classList.remove("menu-expanded")
+  document.body.classList.remove('menu-expanded');
 }
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .cards,
+  #about header,
+  #about .content
+`);
