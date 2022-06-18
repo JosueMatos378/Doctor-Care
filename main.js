e11a67f00c2770986ef81f0549ad0d7dba69a72c
JosueMatos378 <77208952+JosueMatos378@users.noreply.github.com>
2022-06-18 10:58:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,40 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home)
+  // activateMenuAtCurrentSection(services)
+}
+function activateMenuAtCurrentSection(section) {
+  // Linha alvo
+  const targetLine = scrollY + innerHeight / 2
+  // Verificar se a seção passou da linha!
+  // Quais dados vou precisar?
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionHeight
+
+  // Informações dos dados e da lógica!
+  // console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine)
+
+  // Verificar se a base está abaixo da linnha alvo!
+  // Quais dados vou precisar?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // O final da seção passou da linha alvo!
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+  
+  console.log('O fundo da seção passou da linha?', sectionTopReachOrPassedTargetLine)
+
+  // Limites da seção!
+  const sectionBoundaries  = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
+
+  if(sectionBoundaries) {
+  console.log('Estou na seção HOME')
+  }
 }
 function showNavOnScroll() {
-  var element = document.getElementById('navigation');
+  let element = document.getElementById('navigation');
 
   if (scrollY > 0) {
     element.classList.add('scroll');
